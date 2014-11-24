1beb5aa955e8a4b374c2e3b323a5be8ff6d0ae0b
fangtoby <fangtoby@live.cn>
2014-11-24 14:17:24 +0800

    Update Util.js

--- web/js/Util.js
+++ web/js/Util.js
@@ -669,4 +669,55 @@ var Tools = {
 		}
 		return Math.ceil(num);
 	},
+	 /* URL操作
+	 *obj的格式: 
+    	{
+          paramName1: paramValue1
+          [,paramName2: paramValue2...]
+     	}
+    */
+    addQueryParams:function(url, obj) {
+         var key,
+         joinChar = (url.indexOf('?') === -1) ? '?' : '&',
+         arrParams = [],
+         strParams = '';
+         for (key in obj) {
+             arrParams[arrParams.length] = '&' + key + '=' + obj[key];
+         }
+         // 去掉第一个'&', 因为有joinChar存在
+         strParams = arrParams.join('').substring(1);
+          return url + joinChar + strParams;
+    },//获取URL所有参数
+    getUrlParams:function() {
+		var search = window.location.search;
+		// 写入数据字典
+		var tmparray = search.substr(1, search.length).split("&");
+		var paramsArray = new Array;
+		if (tmparray != null) {
+			for (var i = 0; i < tmparray.length; i++) {
+				var reg = /[=|^==]/;    // 用=进行拆分，但不包括==
+				var set1 = tmparray[i].replace(reg, '&');
+				var tmpStr2 = set1.split('&');
+				var array = new Array;
+				array[tmpStr2[0]] = tmpStr2[1];
+				paramsArray.push(array);
+			}
+		}
+		// 将参数数组进行返回
+		return paramsArray;
+	},
+	// 根据参数名称获取参数值
+	getParamValue:function(name) {
+		var paramsArray = getUrlParams();
+		if (paramsArray != null) {
+			for (var i = 0; i < paramsArray.length; i++) {
+				for (var j in paramsArray[i]) {
+					if (j == name) {
+						return paramsArray[i][j];
+					}
+				}
+			}
+		}
+		return null;
+	}
 };
